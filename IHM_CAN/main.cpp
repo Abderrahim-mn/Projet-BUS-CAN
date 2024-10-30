9ab5d7561ded9d0c17025e7965d01701f6774df0
Abderrahim-mn <mounouarabdrrahim2003@gmail.com>
2024-10-30 13:00:07 +0100

    reseau can

---
+++ IHM_CAN/main.cpp
@@ -0,0 +1,15 @@
+/*Author: CHOUBRI Douae*/
+
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.setWindowTitle("Projet Bus Can");
+    w.setWindowFlags(w.windowFlags() & ~Qt::WindowMaximizeButtonHint);
+    w.show();
+    return a.exec();
+}
